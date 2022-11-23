bd63cc5353a9f18fe57f19bc5616108f8e13a187
dat-nguyen-304 <nguyenphatdat3004@gmail.com>
2022-11-24 00:44:09 +0700

    add html-css layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,144 @@
-import './App.css';
+import './App.scss';
 
 function App () {
   return (
     <div className="App">
-      <h1>hello world</h1>
+      <nav className="navbar app">App Bar</nav>
+      <nav className="navbar board">Board Bar</nav>
+      <div className="board-columns">
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://cpopping.info/wp-content/uploads/2021/06/ly-nhat-dong-profile.jpg" alt="" />
+              Title Dat Nguyen
+            </li>
+            <li>Add what you want</li>
+            <li>Add what you want</li>
+            <li>Add what you want</li>
+            <li>Add what you want</li>
+            <li>Add what you want</li>
+            <li>Add what you want</li>
+            <li>Add what you want</li>
+            <li>Add what you want</li>
+            <li>Add what you want</li>
+            <li>Add what you want</li>
+            <li>Add what you want</li>
+
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://cpopping.info/wp-content/uploads/2021/06/ly-nhat-dong-profile.jpg" alt="" />
+              Title Dat Nguyen
+            </li>
+            <li>Add what you want</li>
+            <li>Add what you want</li>
+            <li>Add what you want</li>
+            <li>Add what you want</li>
+            <li>Add what you want</li>
+            <li>Add what you want</li>
+            <li>Add what you want</li>
+            <li>Add what you want</li>
+            <li>Add what you want</li>
+            <li>Add what you want</li>
+            <li>Add what you want</li>
+
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://cpopping.info/wp-content/uploads/2021/06/ly-nhat-dong-profile.jpg" alt="" />
+              Title Dat Nguyen
+            </li>
+            <li>Add what you want</li>
+            <li>Add what you want</li>
+            <li>Add what you want</li>
+            <li>Add what you want</li>
+            <li>Add what you want</li>
+            <li>Add what you want</li>
+            <li>Add what you want</li>
+            <li>Add what you want</li>
+            <li>Add what you want</li>
+            <li>Add what you want</li>
+            <li>Add what you want</li>
+
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://cpopping.info/wp-content/uploads/2021/06/ly-nhat-dong-profile.jpg" alt="" />
+              Title Dat Nguyen
+            </li>
+            <li>Add what you want</li>
+            <li>Add what you want</li>
+            <li>Add what you want</li>
+            <li>Add what you want</li>
+            <li>Add what you want</li>
+            <li>Add what you want</li>
+            <li>Add what you want</li>
+            <li>Add what you want</li>
+            <li>Add what you want</li>
+            <li>Add what you want</li>
+            <li>Add what you want</li>
+
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://cpopping.info/wp-content/uploads/2021/06/ly-nhat-dong-profile.jpg" alt="" />
+              Title Dat Nguyen
+            </li>
+            <li>Add what you want</li>
+            <li>Add what you want</li>
+            <li>Add what you want</li>
+            <li>Add what you want</li>
+            <li>Add what you want</li>
+            <li>Add what you want</li>
+            <li>Add what you want</li>
+            <li>Add what you want</li>
+            <li>Add what you want</li>
+            <li>Add what you want</li>
+            <li>Add what you want</li>
+
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://cpopping.info/wp-content/uploads/2021/06/ly-nhat-dong-profile.jpg" alt="" />
+              Title Dat Nguyen
+            </li>
+            <li>Add what you want</li>
+            <li>Add what you want</li>
+            <li>Add what you want</li>
+            <li>Add what you want</li>
+            <li>Add what you want</li>
+            <li>Add what you want</li>
+            <li>Add what you want</li>
+            <li>Add what you want</li>
+            <li>Add what you want</li>
+            <li>Add what you want</li>
+            <li>Add what you want</li>
+
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+      </div>
     </div>
   );
 }
