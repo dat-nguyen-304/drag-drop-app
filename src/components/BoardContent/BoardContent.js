9b9b8d374728ee7d1f833863bc49b88b74023eef
dat-nguyen-304 <nguyenphatdat3004@gmail.com>
2022-11-24 14:56:04 +0700

    render by static data

--- src/components/BoardContent/BoardContent.js
+++ src/components/BoardContent/BoardContent.js
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Column from '../Column/Column';
 import './BoardContent.scss';
+import { initialData } from '../../actions/initialData';
+import _ from 'lodash';
+import { mapOrder } from '../../utilities/sorts';
 
 function BoardContent () {
+    const [board, setBoard] = useState({});
+    const [columns, setColumns] = useState([]);
+    useEffect(() => {
+        const boardFromDB = initialData.boards.find(board => board.id === 'board-1');
+        if (boardFromDB) {
+            setBoard(boardFromDB);
+            //sort column
+
+            setColumns(mapOrder(boardFromDB.columns, boardFromDB.columnOrder, 'id'));
+        }
+    }, [])
+    if (_.isEmpty(board)) {
+        return <div className="not-found">Board not found</div>
+    }
     return (
         <div className="board-content">
-            <Column />
-            <Column />
-            <Column />
-            <Column />
-            <Column />
-            <Column />
-            <Column />
-            <Column />
+            { columns.map((column, index) => <Column key={ index } column={ column } />) }
         </div>
     )
 }
