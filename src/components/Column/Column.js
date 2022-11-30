c9a079c8a749ca34bf943b7ca2cbb97f542b01e2
dat-nguyen-304 <nguyenphatdat3004@gmail.com>
2022-11-30 16:57:26 +0700

    rename-delete-column

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,18 +1,104 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import './Column.scss';
 import 'font-awesome/css/font-awesome.min.css';
 import Card from '../Card/Card';
+import ConfirmModal from '../Common/ConfirmModal';
+import { Dropdown, Form } from 'react-bootstrap';
 import { mapOrder } from '../../utilities/sorts';
 import { Container, Draggable } from 'react-smooth-dnd';
 
-
 function Column (props) {
-    const { column, onCardDrop } = props;
+    const { column, onCardDrop, saveColumnTitle, removeColumn } = props;
+    const [columnTitle, setColumnTitle] = useState('');
+    const [modalConfirm, setModalConfirm] = useState(false);
     const cards = mapOrder(column.cards, column.cardOrder, 'id');
+    let changeTitleInput = useRef();
+
+    const changeTitleColumn = (e) => {
+        setColumnTitle(e.target.value);
+    }
+
+    useEffect(() => {
+        setColumnTitle(column.title);
+    }, [column.title])
+
+    const saveColumnByPressEnter = (e) => {
+        if (columnTitle) {
+            if (e.key === 'Enter') {
+                e.target.blur();
+                saveColumnTitle({
+                    ...column,
+                    title: columnTitle
+                })
+            }
+        } else {
+            e.target.blur();
+            setColumnTitle(column.title);
+        }
+    }
+
+    const saveColumnByBlur = () => {
+        if (columnTitle) {
+            saveColumnTitle({
+                ...column,
+                title: columnTitle
+            })
+        } else setColumnTitle(column.title);
+    }
+
+    const handleClick = (e) => {
+        e.target.focus();
+    }
+
+    const handleFocus = () => {
+        const end = changeTitleInput.current.value.length;
+        changeTitleInput.current.setSelectionRange(end, end);
+    }
+
+    const toggleConfirmModal = () => {
+        setModalConfirm(!modalConfirm);
+    }
+
+    const handleRemoveColumn = () => {
+        removeColumn(column.id);
+        toggleConfirmModal();
+    }
 
     return (
         <div className="column">
-            <header className="column-drag-handle">{ column.title }</header>
+            <header className="column-drag-handle">
+                <div className="column-header-container">
+                    <Form.Control
+                        ref={ changeTitleInput }
+                        className="column-name-input-editable" size="sm" type="text"
+                        placeholder="Column name..."
+                        value={ columnTitle }
+                        onChange={ changeTitleColumn }
+                        onKeyDown={ (e) => saveColumnByPressEnter(e) }
+                        onBlur={ saveColumnByBlur }
+                        onMouseDown={ e => { e.preventDefault(); e.target.blur(); } }
+                        onClick={ e => handleClick(e) }
+                        onFocus={ handleFocus }
+                    />
+                    <Dropdown>
+                        <Dropdown.Toggle className="column-header-menu" />
+
+                        <Dropdown.Menu>
+                            <Dropdown.Item onClick={ toggleConfirmModal }>Delete Column</Dropdown.Item>
+                            <Dropdown.Item onClick={ () => changeTitleInput.current.focus() }>Rename</Dropdown.Item>
+                            <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+                        </Dropdown.Menu>
+                        <ConfirmModal
+                            showModal={ modalConfirm }
+                            toggleConfirmModal={ toggleConfirmModal }
+                            confirm={ () => handleRemoveColumn() }
+                            title="Confirm delete column"
+                            content={ `Are you sure you want to remove <strong>${columnTitle}</strong>. </br>
+                            All related card will be also deleted.`}
+                        />
+                    </Dropdown>
+                </div>
+            </header >
             <div className="card-list">
                 <Container
                     orientation="vertical"
@@ -38,14 +124,18 @@ function Column (props) {
                     dropPlaceholderAnimationDuration={ 200 }
                 >
                     { cards.map((card, index) => (
-                        <Draggable key={ index }>
-                            <Card card={ card } />
+                        <Draggable key={ card.id }>
+                            <Card key={ card.id } card={ card } />
                         </Draggable>
                     )) }
                 </Container>
             </div>
-            <footer><i className="fa fa-plus icon"></i>Add another task</footer>
-        </div>
+            <footer>
+                <div className="add-new-task-container">
+                    <i className="fa fa-plus icon"></i><span>Add another task</span>
+                </div>
+            </footer>
+        </div >
     )
 }
 
